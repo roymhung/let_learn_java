59b8f44f310f8df424edaabff140fab1aef4c048
royhung <lemanhhung1555@gmail.com>
2025-09-06 17:20:33 +0700

    chapter2 - Lab04 all version important

--- java-core-royhung/src/chapter2/Lab_04.java
+++ java-core-royhung/src/chapter2/Lab_04.java
@@ -15,12 +15,16 @@ public class Lab_04 {
 
 		System.out.println("--------------------------");
 
-		for (int i = 1; i <= 5; i++) {
+		for (int i = 1; i <= 10; i++) {
 			if (i == 3)
 				continue;
 			System.out.println(i);
 		}
+		System.out.println("--------------------------");
 
+		System.out.println(i);
+
+		System.out.println("--------------------------");
 	}
 
 }
