b70b756e2e36eb538f73b7aa108c6344a081d30a
royhung <lemanhhung1555@gmail.com>
2025-09-07 08:58:32 +0700

    chapter3 lab05

--- java-core-royhung/src/chapter3/Lab_05.java
+++ java-core-royhung/src/chapter3/Lab_05.java
@@ -1,5 +1,13 @@
 package chapter3;
 
 public class Lab_05 {
+	public static void main(String[] args) {
+		String name = "royhung haha 1";
 
+		System.out.println("length: " + name.length());
+		System.out.println("length: " + name.toUpperCase());
+		System.out.println("length: " + name.toLowerCase());
+		System.out.println(name.substring(0, 3));
+
+	}
 }
