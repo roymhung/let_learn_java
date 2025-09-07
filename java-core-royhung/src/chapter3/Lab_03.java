4e04e02b984f0ecb3fe2ff39f4928fb8e2239548
royhung <lemanhhung1555@gmail.com>
2025-09-07 08:13:09 +0700

    chapter3 lab03

--- java-core-royhung/src/chapter3/Lab_03.java
+++ java-core-royhung/src/chapter3/Lab_03.java
@@ -1,5 +1,14 @@
 package chapter3;
 
 public class Lab_03 {
+	public static void main(String[] args) {
+		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println(); // empty line
+		}
 
+	}
 }
