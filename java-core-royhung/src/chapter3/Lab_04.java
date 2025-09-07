f5b38562be3f0c3d5e363eee8f5b7b1d722afd37
royhung <lemanhhung1555@gmail.com>
2025-09-07 08:19:33 +0700

    chapter3 lab04

--- java-core-royhung/src/chapter3/Lab_04.java
+++ java-core-royhung/src/chapter3/Lab_04.java
@@ -1,5 +1,10 @@
 package chapter3;
 
 public class Lab_04 {
-
+	public static void main(String[] args) {
+		String[] country = { "vietnam", "lao", "campuchia", "thailan" };
+		for (String item : country) {
+			System.out.println("item = " + item);
+		}
+	}
 }
