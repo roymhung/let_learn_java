75861e85dc5a7dec94ee3d8a64fadcb11c8ac2d1
royhung <lemanhhung1555@gmail.com>
2025-09-07 10:08:53 +0700

    chapter3 lab07

--- java-core-royhung/src/chapter3/Lab_07.java
+++ java-core-royhung/src/chapter3/Lab_07.java
@@ -1,5 +1,53 @@
 package chapter3;
 
-public class Lab_07 {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Lab_07 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+
+		// Yêu cầu: Viết chương trình Java thực hiện các bước sau:
+		// Nhập vào tên và điểm của 3 học sinh.
+		// Lưu tên vào mảng String[ ], điểm vào mảng double[ ].
+		// In ra danh sách học sinh và điểm tương ứng.
+		// Tính và in ra điểm trung bình của cả lớp.
+		// In ra học sinh có điểm từ 8 trở lên.
+
+		String[] names = new String[3];
+		double[] scores = new double[3];
+
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("Nhap vao ten cua hoc sinh " + i + ":");
+			String name = scanner.nextLine();
+			names[i - 1] = name;
+
+			System.out.println("Nhap vao diem cua hoc sinh " + i + ":");
+			double score = scanner.nextDouble();
+			scores[i - 1] = score;
+
+			scanner.nextLine();
+		}
+
+		System.out.println("-----------------------");
+		System.out.println("Danh sach da nhap: ");
+		double sum = 0;
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("ten = " + names[i - 1] + " va diem = " + scores[i - 1]);
+
+			sum += scores[i - 1];
+		}
+		System.out.printf("Diem trung binh: %.2f", sum / 3);
+		System.out.println();
+		System.out.println("-----------------------");
+
+		System.out.println("Diem lon hon 8: ");
+		for (int i = 1; i <= 3; i++) {
+			if (scores[i - 1] >= 8) {
+				System.out.println("ten = " + names[i - 1] + " va diem = " + scores[i - 1]);
+			}
+		}
+		scanner.close();
+	}
 }
