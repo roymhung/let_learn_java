d4a2a88e0fad01ac564b2a388233b0e9eed2e843
royhung <lemanhhung1555@gmail.com>
2025-09-11 00:01:20 +0700

    chapter7 demo static/final

---
+++ java-core-royhung/src/chapter7/video87/Student.java
@@ -0,0 +1,7 @@
+package chapter7.video87;
+
+public class Student {
+
+	public final String country = "VIETNAM";
+
+}
---
+++ java-core-royhung/src/chapter7/video87/Video87.java
@@ -0,0 +1,9 @@
+package chapter7.video87;
+
+public class Video87 {
+	public static void main(String[] args) {
+		System.out.println("----------------");
+		Student st = new Student();
+		System.out.println("Check country = " + st.country);
+	}
+}
