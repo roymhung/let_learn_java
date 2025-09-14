eb412129b72d9b1d98527dc1b8d45123bcaf1600
royhung <lemanhhung1555@gmail.com>
2025-09-15 04:44:05 +0700

    chapter9 List vs ArrayList

---
+++ java-core-royhung/src/chapter9/Video109.java
@@ -0,0 +1,30 @@
+package chapter9;
+
+import java.util.ArrayList;
+
+public class Video109 {
+	public static void main(String[] args) {
+		System.out.println("----------------------");
+
+		ArrayList<String> royhung = new ArrayList();
+		royhung.add("ha noi"); // ["hanoi"]
+		royhung.add("hcm"); // ["hanoi", "hcm"]
+		royhung.add("da nang"); // ["hanoi","hcm", "da nang"]
+		royhung.add("ha noi"); // ["hanoi", "hcm", "da nang", "hanoi"]
+
+		for (String city : royhung) {
+			System.out.println(city);
+		}
+		// ArrayList<int> test = new ArrayList<>(); tại sao cái này sai
+
+		ArrayList<Integer> test = new ArrayList<>(); // còn cái này đúng
+
+		test.add(1);// so 1 da duoc chuyen tu kieu du lieu nguyen thuy sang kieu obj(tham chieu)
+		// dac diem khi ta thao tac voi obj thi ta phai viet hoa
+		test.add(0);
+
+		for (int number : test) {
+			System.out.println(number);
+		}
+	}
+}
